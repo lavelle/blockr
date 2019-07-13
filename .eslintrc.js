7d1394c874a77093c0e273dd5a0acad688be3ace
Giles Lavelle <giles.lavelle@gmail.com>
2019-07-13 13:22:40 -0700

    Some stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,6 @@
 module.exports = {
     extends: ['airbnb', 'prettier'],
+    rules: {
+        'no-console': 'off',
+    },
 };
--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const meow = require('meow');
 const cli = meow(
     `
 	Usage
-	  $ blocko
+      $ blocko
+      $ blocko unblock
 
 	Options
 	  --hosts-file, -h  Path to the hosts file
@@ -41,23 +42,60 @@ function generateBlockString(hosts) {
 
 const config = JSON.parse(fs.readFileSync(path.join(__dirname, 'blocko.json'), 'utf-8'));
 
-// fs.closeSync(fs.openSync(cli.flags.hostsFile, 'w'));
-
-const currentHosts = fs.readFileSync(cli.flags.hostsFile, 'utf-8');
-
-const blockString = generateBlockString(config.hosts);
-
-const startLocation = currentHosts.indexOf(BEGIN_MARKER);
-const endLocation = currentHosts.indexOf(END_MARKER);
-
-let newHosts;
-if (startLocation > -1 && endLocation > -1) {
-    newHosts =
-        currentHosts.substr(0, startLocation) +
-        blockString +
-        currentHosts.substr(endLocation + END_MARKER.length);
-} else {
-    newHosts = `${currentHosts}\n${blockString}`;
+function tryWrite(hosts) {
+    try {
+        fs.writeFileSync(cli.flags.hostsFile, hosts);
+    } catch (error) {
+        if (error.code === 'EACCES') {
+            console.log('Please re-run with sudo to write to the hosts file');
+        }
+        process.exit(1);
+    }
 }
 
-fs.writeFileSync(cli.flags.hostsFile, newHosts);
+function addBlock() {
+    if (!fs.existsSync(cli.flags.hostsFile)) {
+        fs.closeSync(fs.openSync(cli.flags.hostsFile, 'w'));
+    }
+    const currentHosts = fs.readFileSync(cli.flags.hostsFile, 'utf-8');
+    const blockString = generateBlockString(config.hosts);
+
+    const startLocation = currentHosts.indexOf(BEGIN_MARKER);
+    const endLocation = currentHosts.indexOf(END_MARKER);
+
+    let newHosts;
+    if (startLocation > -1 && endLocation > -1) {
+        newHosts =
+            currentHosts.substr(0, startLocation) +
+            blockString +
+            currentHosts.substr(endLocation + END_MARKER.length);
+    } else {
+        newHosts = `${currentHosts}\n${blockString}`;
+    }
+
+    tryWrite(newHosts);
+}
+
+function removeBlock() {
+    const currentHosts = fs.readFileSync(cli.flags.hostsFile, 'utf-8');
+    const startLocation = currentHosts.indexOf(BEGIN_MARKER);
+    const endLocation = currentHosts.indexOf(END_MARKER);
+
+    if (startLocation > -1 && endLocation > -1) {
+        const newHosts =
+            currentHosts.substr(0, startLocation) +
+            currentHosts.substr(endLocation + END_MARKER.length);
+
+        tryWrite(newHosts);
+    }
+}
+
+const command = cli.input[0];
+
+if (!command || command === 'block') {
+    addBlock();
+}
+
+if (command === 'unblock') {
+    removeBlock();
+}
